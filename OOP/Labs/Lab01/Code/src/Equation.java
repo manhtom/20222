e5f22f87fc793e02dcc9ff7091b44518bcaa611b
D <manhtom2003@gmail.com>
2023-04-10 15:40:30 +0700

    update

--- OOP/Labs/Lab01/Code/src/Equation.java
+++ OOP/Labs/Lab01/Code/src/Equation.java
@@ -4,8 +4,8 @@ import java.util.*;
 public class Eqn{
     public static void main(String[] args){
         String option="";
-        while (option.equals("1") == false && option.equals("2") == false){
-            option=JOptionPane.showInputDialog(null, "Select 1 if you want to solve single variable equation, select 2 if you want to solve a system of equations");
+        while (option.equals("1") == false && option.equals("2") == false && option.equals("3") == false){
+            option=JOptionPane.showInputDialog(null, "Select 1 if you want to solve single variable 1st-deg equation.\nSelect 2 if you want to solve a system of 1st-deg equations with 2 variables.\nSelect 3 if you want to solve single variable 2nd-deg equation.");
         }
 
         if (option.equals("1")){
@@ -13,6 +13,9 @@ public class Eqn{
         }
         else if (option.equals("2")){
             System.out.println(FirstDegreeSys());
+        }
+        else{
+            JOptionPane.showMessageDialog(null,SecondDegree());
         }
     }
     public static String FirstDegree(){
@@ -56,5 +59,21 @@ public class Eqn{
         else{
             return "Single solution: "+String.format("(%.2f, %.2f)",Dx/D,Dy/D);
         }
+    }
+    public static String SecondDegree(){ //this method is implemented with javax.swing
+        double a=Double.parseDouble(JOptionPane.showInputDialog(null,"Please enter the first coefficient: "));
+        double b=Double.parseDouble(JOptionPane.showInputDialog(null,"Please enter the second coefficient: "));
+        double c=Double.parseDouble(JOptionPane.showInputDialog(null,"Please enter the third coefficient: "));
+        double delta=Math.pow(b,2)-4*a*c; //math.pow chu deo phai math.exp nhe (exp la e roi :)). math.power operates in double mode
+        if (delta<0){
+            return "No solution";
+        }
+        else if (delta==0){
+            return "Double solution: "+String.format("%.2f",-b/(2*a));
+        }
+        else{
+            return "Two unique solutions: "+String.format("(%.2f, %.2f)",(-b+Math.sqrt(delta))/(2*a), (-b-Math.sqrt(delta))/(2*a));
+        }
+
     }
 }
