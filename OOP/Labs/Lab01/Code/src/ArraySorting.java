97ab01be28ad4def28f73a7f5a2faec05b691d3a
D <manhtom2003@gmail.com>
2023-04-11 09:29:00 +0700

    Update ArraySorting.java

--- OOP/Labs/Lab01/Code/src/ArraySorting.java
+++ OOP/Labs/Lab01/Code/src/ArraySorting.java
@@ -1,3 +1,22 @@
 public class ArraySorting {
-    
+   	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner s = new Scanner(System.in);
+		System.out.printf("%s","Enter the array size: ");
+		int n = s.nextInt();
+		s.nextLine();
+		int[] array = new int[n];
+		System.out.printf("%s","Enter the array: ");
+		int k = 0;
+		while (s.hasNextInt() && k<n) {
+			int input = s.nextInt();
+			array[k] = input;
+			k++;
+		}
+		s.close();
+		// done inputting. now still needs to sort
+		//System.out.println("The sum of array elements is "+)
+		//System.out.println("The average of array elements is "+)
+
+	} 
 }
