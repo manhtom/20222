4239a9dd009880c69b46eb6c1870e4e0d843c09b
D <manhtom2003@gmail.com>
2023-04-28 12:30:58 +0700

    updt

--- OOP/Labs/OOP.20221.20214949.DMC/Triangle/src/App.java
+++ OOP/Labs/OOP.20221.20214949.DMC/Triangle/src/App.java
@@ -6,12 +6,13 @@ public class App {
         int n = s.nextInt();
         s.close();
         for (int k=0; k < n; k++) { // loop k lan
-            String d="%"+(int)((2*n-1)/2-k+1)+"s"; //cong thuc bai nao
+            System.out.println(n-k-1+" "+2*k+2);
+            String d="%"+(n-k-1)+"s"; //cong thuc bai nao
             System.out.printf(d,"");
             for (int j=1; j <= 2*(k+1)-1; j++){ // loop 2k-1 lan
                 System.out.printf("%s","*");
             }
-            System.out.println(""); // break line
+            //System.out.println(""); // break line
         }
     }
 }
